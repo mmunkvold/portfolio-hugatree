ec7d0f09de9ac405b8493bde952adec792509e61
mmunkvold <mmunkvold@yahoo.no>
2021-09-16 15:16:25 +0200

    Updated

--- js/blogposts.js
+++ js/blogposts.js
@@ -1,28 +1,29 @@
 //this code needs work, but just to show how to
 
-const url = 'https://monicamunkvoldnikolaisen.no/treehugging/wp-json/wp/v2/posts?per_page=100&_embed';
+const url = "https://monicamunkvoldnikolaisen.no/treehugging/wp-json/wp/v2/posts?per_page=100&_embed";
 
-const resultsContainer = document.querySelector('.results');
-const button = document.querySelector('button');
+const resultsContainer = document.querySelector(".results");
+const button = document.querySelector(".seemore-btn");
+console.log(button);
 
 async function getAllPosts() {
   try {
     const response = await fetch(url);
     const results = await response.json();
-    resultsContainer.innerHTML = '';
+    resultsContainer.innerHTML = "";
 
     for (let i = 0; i < results.length; i++) {
       if (i === 10) {
         break;
       }
-      resultsContainer.innerHTML += `<div class="results"><a href="singlepost.html?id=${results[i].id}"><div class="">${results[i].title.rendered} </div><img class="" src="${results[i]._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url}"></img></a><div>${results[i].excerpt.rendered}</div></div>`;
+      resultsContainer.innerHTML += `<div class="results"><div class="result-item"><a href="singlepost.html?id=${results[i].id}"><div><img  src="${results[i]._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url}"></img></div><div class="">${results[i].title.rendered} </a>${results[i].excerpt.rendered}</div></div></div>`;
     }
-    button.addEventListener('click', function () {
+    button.addEventListener("click", function () {
       console.log("i've been clicked");
       for (let i = 11; i < results.length; i++) {
-        console.log('yayy');
-        resultsContainer.innerHTML += `<div class="results"><a href="singlepost.html?id=${results[i].id}"><div class="">${results[i].title.rendered} </div><img class="" src="${results[i]._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url}"></img></a><div>${results[i].excerpt.rendered}</div></div>`;
-        button.style.display = 'none';
+        console.log("yayy");
+        resultsContainer.innerHTML += `<div class="results"><div class="result-item"><a href="singlepost.html?id=${results[i].id}"><div><img  src="${results[i]._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url}"></img></div><div class="">${results[i].title.rendered} </a>${results[i].excerpt.rendered}</div></div></div>`;
+        button.style.display = "none";
       }
     });
   } catch (error) {
