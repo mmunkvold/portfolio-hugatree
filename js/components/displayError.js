ebf9d9fef2a88d255f9abb6c7fd1d8a0874c4267
Monica M Nikolaisen <70950983+mmunkvold@users.noreply.github.com>
2021-09-27 11:53:45 +0200

    Delete js directory

--- js/components/displayError.js
+++
@@ -1,3 +0,0 @@
-function displayError(message = "Error Unknown") {
-  return `<div class="error-message">${message}</div>`;
-}
--- js/components/to-topbutton.js
+++
@@ -1,6 +0,0 @@
-const toTopButton = document.getElementById("totop");
-console.log(toTopButton);
-
-toTopButton.onclick = function () {
-  window.scrollTo(0, 0);
-};
